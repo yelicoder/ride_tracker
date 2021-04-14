fe55c649796dc1819548b1b056a5fab90cbf59e2
ye Li <yeli.88@gmail.com>
2021-04-14 18:15:51 -0400

    completed module6

---
+++ module6/src/main/java/com/pluralsight/repository/RideRepository.java
@@ -0,0 +1,15 @@
+package com.pluralsight.repository;
+
+import java.util.List;
+
+import com.pluralsight.model.Ride;
+
+public interface RideRepository {
+
+	List<Ride> getRides();
+	Ride createRide(Ride ride);
+	Ride getRide(Integer id);
+	Ride updateRide(Ride ride);
+	void updaterRides(List<Object[]> pairs); 
+
+}
---
+++ module6/src/main/java/com/pluralsight/service/RideService.java
@@ -0,0 +1,14 @@
+package com.pluralsight.service;
+
+import java.util.List;
+
+import com.pluralsight.model.Ride;
+
+public interface RideService {
+
+	List<Ride> getRides();
+	Ride createRide(Ride ride);
+	Ride getRide(Integer id);
+	Ride updateRide(Ride ride);
+	void batch();
+}
